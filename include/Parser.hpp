639e421ac7fc5050b68de680232190e4458e926f
cgangaro <rocket4duck@hotmail.com>
2025-03-04 11:07:25 +0100

    Begin parser but wrong way.

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -1,33 +1,27 @@
 #pragma once
+
 #include <string>
 #include <vector>
-#include <map>
-using namespace std;
-
-struct Process
-{
-	string name;
-	map<string, string> inputs;
-	map<string, string> outputs;
-	unsigned int time;
-};
+#include <unordered_map>
+#include "Stock.hpp"
+#include "Process.hpp"
 
 class Parser
 {
 public:
-	Parser(string &filename);
-	~Parser();
+	Parser(const std::string &filename);
+	bool parse();
 
-	const map<string, int> &getStocks() const;
-	const vector<Process> &getProcesses() const;
-	const vector<string> &getOptimizeGoals() const;
+	const std::vector<Stock> &getStocks() const;
+	const std::vector<Process> &getProcesses() const;
 
 private:
-	void parseLine(string &line);
+	std::string _filename;
+	std::vector<Stock> _stocks;
+	std::vector<Process> _processes;
 
-	map<string, int> stocks;
-	vector<Process> processes;
-	vector<string> optimizeGoals;
-	enum class Section { STOCKS, PROCESSES, OPTIMIZE_GOALS };
-	Section currentSection;
+	void parseStock(const std::string &line);
+	void parseProcess(const std::string &line);
+	void parseResources(const std::string &token, std::unordered_map<std::string, int> &resources);
+	void parseOptimization(const std::string &line);
 };
