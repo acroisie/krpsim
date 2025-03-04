97052140e2dfce2ddd529c6e13f2fffd3b474a7a
cgangaro <rocket4duck@hotmail.com>
2025-03-04 15:36:01 +0100

    Wip on parsing

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,69 +1,249 @@
-#pragma once
-
 #include "Parser.hpp"
-#include "StringUtils.hpp"
-#include <fstream>
+#include "Stock.hpp"
+#include "Process.hpp"
 #include <iostream>
-#include <algorithm>
+#include <fstream>
+#include <stdexcept>
+#include <string>
+#include <sstream>
 
-using namespace std;
-
-Parser::Parser(const string &filename) : _filename(filename) {}
-
-bool Parser::parse()
+class LineParser
 {
-	ifstream file{_filename};
-	if (!file)
+public:
+	explicit LineParser(const std::string &input) : _input(input), _pos(0) {}
+
+	void skipWhithespace()
 	{
-		cerr << "Error: could not open file " << _filename << endl;
+		while (_pos < _input.size() && std::isspace(_input[_pos]))
+		{
+			++_pos;
+		}
+	}
+
+	char peek()
+	{
+		skipWhithespace();
+		return _pos < _input.size() ? _input[_pos] : '\0';
+	}
+
+	void expect(char c)
+	{
+		skipWhithespace();
+		if (peek() != c)
+		{
+			throw std::runtime_error("Unexpected character");
+		}
+		++_pos;
+	}
+
+	bool match(char c)
+	{
+		skipWhithespace();
+		if (peek() == c)
+		{
+			++_pos;
+			return true;
+		}
 		return false;
 	}
 
-	string line;
-
-	while (getline(file, line))
+	std::string parseIdentifier()
 	{
-		line = trim(line);
-		if (line.empty() || line.front() == '#')
+		skipWhithespace();
+		size_t start = _pos;
+		while (_pos < _input.size() && (std::isalnum(_input[_pos]) || _input[_pos] == '_' || _input[_pos] == '-'))
+		{
+			++_pos;
+		}
+		if (start == _pos)
+		{
+			throw std::runtime_error("Expected identifier");
+		}
+		return _input.substr(start, _pos - start);
+	}
+
+	int parseInteger()
+	{
+		skipWhithespace();
+		size_t start = _pos;
+		if (_pos < _input.size() && (_input[_pos] == '+' || _input[_pos] == '-'))
+		{
+			++_pos;
+		}
+		while (_pos < _input.size() && std::isdigit(_input[_pos]))
+		{
+			++_pos;
+		}
+		if (start == _pos)
+		{
+			throw std::runtime_error("Expected integer");
+		}
+		try
+		{
+			return std::stoi(_input.substr(start, _pos - start));
+		}
+		catch (const std::exception &e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+
+		return std::stoi(_input.substr(start, _pos - start));
+	}
+
+	bool eof() const
+	{
+		return _pos >= _input.size();
+	}
+
+private:
+	const std::string &_input;
+	size_t _pos;
+};
+
+std::unordered_map<std::string, int> parseResourceList(LineParser &parser)
+{
+	std::unordered_map<std::string, int> resources;
+	while (parser.eof() == false)
+	{
+		parser.skipWhithespace();
+		if (parser.peek() == ')')
+		{
+			break;
+		}
+		std::string resource = parser.parseIdentifier();
+		parser.skipWhithespace();
+		parser.expect(':');
+		parser.skipWhithespace();
+		int amount = parser.parseInteger();
+		resources[resource] = amount;
+		parser.skipWhithespace();
+
+		if (parser.match(';'))
+		{
+			break;
+		}
+	}
+	return resources;
+}
+
+std::vector<std::string> parseOptimizeLine(LineParser &parser)
+{
+	std::vector<std::string> optimize;
+	while (parser.eof() == false)
+	{
+		parser.skipWhithespace();
+		if (parser.peek() == ')')
+		{
+			break;
+		}
+		optimize.push_back(parser.parseIdentifier());
+		parser.skipWhithespace();
+		if (parser.match(';'))
+		{
+			break;
+		}
+	}
+	return optimize;
+}
+
+Parser::Parser(const std::string &filename) : _filename(filename) {}
+
+bool Parser::parse()
+{
+	std::ifstream file(_filename);
+	if (!file)
+	{
+		std::cerr << "Error: could not open file " << _filename << std::endl;
+		return false;
+	}
+	std::string line;
+	int lineNumber = 0;
+
+	while (std::getline(file, line))
+	{
+		++lineNumber;
+		std::istringstream stream(line);
+		std::string trimmed;
+		std::getline(stream, trimmed);
+		std::string temp = trimmed;
+		size_t pos = temp.find_first_not_of(" \t");
+		if (pos == std::string::npos || temp[pos] == '#')
 		{
 			continue;
 		}
-
-		if (line.find("optimize:") != string::npos)
+		try
 		{
-			parseOptimization(line);
+			parseLine(trimmed, lineNumber);
 		}
-		else if (line.find("stock:") != string::npos)
+		catch (const std::exception &e)
 		{
-			parseStock(line);
-		}
-		else if (line.find("process:") != string::npos)
-		{
-			parseProcess(line);
-		}
-		else
-		{
-			cerr << "Error: unknown token " << line << endl;
+			std::cerr << "Error in line " << lineNumber << ": " << e.what() << std::endl;
 			return false;
 		}
 	}
 	return true;
 }
 
-const vector<Stock> &Parser::getStocks() const
+const std::vector<Stock> &Parser::getStocks() const
 {
 	return _stocks;
 }
 
-const vector<Process> &Parser::getProcesses() const
+const std::vector<Process> &Parser::getProcesses() const
 {
 	return _processes;
 }
 
-void Parser::parseStock(const string& line) {
-	auto elements = split(line, ':');
-	if (elements.size() == 2) {
-		cout << "Stock: " << elements[1] << endl; // TODO: remove this line
-		_stocks.push_back(elements)
+void Parser::parseLine(const std::string &line, int lineNumber)
+{
+	LineParser parser(line);
+	parser.skipWhithespace();
+
+	std::string firstToken = parser.parseIdentifier();
+
+	if (firstToken == "optimize")
+	{
+		parser.skipWhithespace();
+		parser.expect(':');
+		parser.skipWhithespace();
+		parser.expect('(');
+
+		auto optimize = parseOptimizeLine(parser);
+
+		parser.expect(')');
 	}
-}
+	else
+	{
+		parser.skipWhithespace();
+		parser.expect(':');
+		parser.skipWhithespace();
+
+		if (parser.peek() == '(')
+		{
+			Process process;
+			process.name = firstToken;
+			parser.expect('(');
+			process.inputs = parseResourceList(parser);
+			parser.expect(')');
+			parser.skipWhithespace();
+			parser.expect(':');
+			parser.skipWhithespace();
+			parser.expect('(');
+			process.outputs = parseResourceList(parser);
+			parser.expect(')');
+			parser.skipWhithespace();
+			parser.expect(':');
+			parser.skipWhithespace();
+			process.cycleAmount = parser.parseInteger();
+			_processes.push_back(process);
+		}
+		else
+		{
+			Stock stock;
+			stock.name = firstToken;
+			parser.expect('(');
+			stock.quantity = parser.parseInteger();
+			parser.expect(')');
+			_stocks.push_back(stock);
+		}
+	}
